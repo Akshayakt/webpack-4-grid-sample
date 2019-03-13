92fdfe0fbd4d03f71eebd8e09b35f204ce1d7039
akshaya <akshaya@qburst.com>
2019-03-13 11:15:06 +0530

    adding folders and files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./scss/main.scss";
+import tvImg from './assets/tv.png';
+import floppyImg from './assets/floppy.png';
+import twitterImg from './assets/twitter.png';
+
+var tvIcon = document.getElementById('tvIcon'),
+  floppyIcon = document.getElementById('floppyIcon'),
+  twitterIcon = document.getElementById('twitterIcon');
+
+tvIcon.src = tvImg;
+floppyIcon.src = floppyImg;
+twitterIcon.src = twitterImg;
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+// webpack v4
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require("webpack-md5-hash");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use:  [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.[contenthash].css"
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+};
