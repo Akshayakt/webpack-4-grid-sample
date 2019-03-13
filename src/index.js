b7dc0b0d4142e9739ab713cb5fa6cb270e9c912f
akshaya <akshaya@qburst.com>
2019-03-13 17:12:07 +0530

    adding complete blocks

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import "./scss/main.scss";
 import tvImg from './assets/tv.png';
 import floppyImg from './assets/floppy.png';
 import twitterImg from './assets/twitter.png';
+import iPadImg from './assets/iPad_Pro.png';
+import gameImg from './assets/games.png';
+import mactiImg from './assets/macintosh.png';
+import keyBoardImg from './assets/keyboard.png';
+import iphoneImg from './assets/iphone.png';
 
 var tvIcon = document.getElementById('tvIcon'),
   floppyIcon = document.getElementById('floppyIcon'),
-  twitterIcon = document.getElementById('twitterIcon');
+  twitterIcon = document.getElementById('twitterIcon'),
+  iPadIcon = document.getElementById('iPadIcon'),
+  gameIcon = document.getElementById('gameIcon'),
+  mactiIcon = document.getElementById('mactiIcon'),
+  keyBoardIcon = document.getElementById('keyBoardIcon'),
+  iphoneIcon = document.getElementById('iphoneIcon');
 
 tvIcon.src = tvImg;
 floppyIcon.src = floppyImg;
 twitterIcon.src = twitterImg;
+iPadIcon.src = iPadImg;
+gameIcon.src = gameImg;
+mactiIcon.src = mactiImg;
+keyBoardIcon.src = keyBoardImg;
+iphoneIcon.src = iphoneImg;
